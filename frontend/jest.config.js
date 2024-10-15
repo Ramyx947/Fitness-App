ea41545e379edd4f8c81a16cb4a8868b6cd8f292
jessgilbert-sbg <124255477+jessgilbert-sbg@users.noreply.github.com>
2024-10-15 22:51:52 +0100

    [SCRUM-75] congifure Jest

---
+++ frontend/jest.config.js
@@ -0,0 +1,12 @@
+export default {
+  collectCoverageFrom: ["src/**/*.{js,jsx}"],
+  coverageDirectory: "coverage",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/jest.setup.js"],
+  moduleNameMapper: {
+    "^.+\\.(css|png)$": "<rootDir>/src/jest-stub.js",
+  },
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+};
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Renders App Header", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/MLA Fitness App/i);
+  expect(headerElement).toBeInTheDocument();
 });
