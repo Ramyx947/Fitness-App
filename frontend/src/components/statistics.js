2a1a8c918942fd54cc807cb38b7242ed26ff8832
diana-create <dianamuse9@gmail.com>
2024-10-19 16:06:33 +0100

    feat: change statistics endpoint for frontend to use graphql

--- frontend/src/components/statistics.js
+++ frontend/src/components/statistics.js
@@ -5,18 +5,46 @@ import "./statistics.css";
 
 const Statistics = ({ currentUser }) => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null)
 
   useEffect(() => {
-    const url = `http://localhost:5050/stats/${currentUser}`;
+    const url = `http://localhost:5050/api/graphql`;
+    const query = `
+    query {
+      filteredStats(name: "${currentUser}") {
+        success
+        errors
+        results {
+            username
+            exercises {
+                exerciseType
+                totalDuration
+           } }
+    } }
+  `;
+
+    console.log(`Sending request to ${url} with query:`, query); // Log before the request
 
     axios
-      .get(url)
+      .post(url, {
+        query: query,
+      })
       .then((response) => {
-        setData(response.data.stats);
+        const result = response.data.data.filteredStats;
+        if (result.success) {
+          setData(result.results);
+        } else {
+          setError(result.errors);
+        }
       })
       .catch((error) => {
-        console.error("There was an error fetching the data!", error);
-      });
+        console.error("There was an error fetching the data", error);
+        setError("Network or sever error");
+      })
+      .finally(() => {
+        setLoading(false);
+      })
   }, [currentUser]);
 
   const currentUserData = data.find((item) => item.username === currentUser);
@@ -24,7 +52,11 @@ const Statistics = ({ currentUser }) => {
   return (
     <div className="stats-container">
       <h4>Well done, {currentUser}! This is your overall effort:</h4>
-      {currentUserData ? (
+      {loading ? (
+        <p>Loading data...</p>
+      ) : error ? (
+        <p>Error: {error}</p>
+      ) : currentUserData ? (
         currentUserData.exercises.map((item, index) => (
           <div key={index} className="exercise-data">
             <div>
