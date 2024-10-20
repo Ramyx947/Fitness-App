e3957fb841c2498dd484446a39dc989697e94a8b
diana-create <dianamuse9@gmail.com>
2024-10-20 19:43:41 +0100

    feat: sample

--- recipes/app.py
+++ recipes/app.py
@@ -7,8 +7,8 @@ from bson import json_util
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}},
-     methods="GET,HEAD,POST,OPTIONS,PUT,PATCH,DELETE")
+# CORS(app, resources={r"/*": {"origins": "*"}},
+     # methods="GET,HEAD,POST,OPTIONS,PUT,PATCH,DELETE")
 
 load_dotenv()
 mongo_uri = os.getenv('MOGO_URI')
@@ -17,11 +17,9 @@ mongo_db = os.getenv('MONGO_DB')
 client = MongoClient(mongo_uri)
 db = client[mongo_db]
 
-@app.route('/')
+@app.route("/")
 def index():
-    recipes = db.recipes.find()
-    recipes_list = list(recipes)
-    return json_util.dumps(recipes_list)
+    return "<p>Hi and welcome to the recipes page!</p>"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5051)
