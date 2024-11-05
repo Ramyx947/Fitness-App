6f0833846922c48728c3dd1ba19059c6fe6f4999
Ramona <ramona.mironescu@gmail.com>
2024-11-05 20:40:28 +0000

    Add Recipes unit tests

---
+++ recipes/tests/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from unittest.mock import patch
+import mongomock
+
+@pytest.fixture(scope='session')
+def mock_mongo():
+    # Patch 'pymongo.MongoClient' before 'app' is imported
+    with patch('pymongo.MongoClient', new=mongomock.MongoClient):
+        yield mongomock.MongoClient()
+
+@pytest.fixture
+def client(mock_mongo):
+    from app import app as flask_app  # Import after patching
+    with flask_app.test_client() as client:
+        yield client
